f952e7890ecccd566c21c45cf6d0d57a911106c1
SmokingIsBadMkay <timvandijkhuizen@hotmail.nl>
2019-12-09 10:38:27 +0100

    Added border to location button for testing

--- location_button.py
+++ location_button.py
@@ -10,7 +10,11 @@ class LocationButton(Clickable):
     def setup(self):
         if self.location == None:
             raise ValueError('Location must be set')
-            
+         
+    def draw(self):
+        fill(0, 0, 0, 0)
+        rect(self.x, self.y, self.width, self.height)
+                  
     def callback(self):
         playerManager = self.app.getModule('playerManager')
         locationScreen = self.app.getScreen('location')
