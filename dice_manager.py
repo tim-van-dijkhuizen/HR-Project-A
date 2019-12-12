a28be720296fea1905a54dcc6e6b211812a77582
SmokingIsBadMkay <timvandijkhuizen@hotmail.nl>
2019-12-12 13:32:30 +0100

    Cleaned DiceManager

--- dice_manager.py
+++ dice_manager.py
@@ -2,35 +2,27 @@ from module import Module
 
 class DiceManager(Module):
     
-    imgDice1 = None
-    imgDice2 = None
-    imgDice3 = None
-    imgDice4 = None
-    imgDice5 = None
-    imgDice6 = None
+    diceImageOne = None
+    diceImageTwo = None
+    diceImageThree = None
+    diceImageFour = None
+    diceImageFive = None
+    diceImageSix = None
     
-    rand = 0
-    value = 0
+    diceValue = 0
     
     def setup(self):
-        self.imgDice1 = loadImage("dice1.png")
-        self.imgDice2 = loadImage("dice2.png")
-        self.imgDice3 = loadImage("dice3.png")
-        self.imgDice4 = loadImage("dice4.png")
-        self.imgDice5 = loadImage("dice5.png")
-        self.imgDice6 = loadImage("dice6.png")
+        self.diceImageOne = loadImage("dice1.png")
+        self.diceImageTwo = loadImage("dice2.png")
+        self.diceImageThree = loadImage("dice3.png")
+        self.diceImageFour = loadImage("dice4.png")
+        self.diceImageFive = loadImage("dice5.png")
+        self.diceImageSix = loadImage("dice6.png")
                      
     def getHandle(self):
        return 'diceManager'
-   
-    def rollDice(self):
-        pass
         
-    def mouseClicked(): 
-        global value
-        if value == 0:
-            global rand
-            rand = int(random(6)) + 1
-        else:
-            rand = int(random(6)) + 1
+    def mousePressed(self): 
+        self.diceValue = int(random(1, 7))
+        print('Dice Value:', self.diceValue)
         
