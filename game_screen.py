877254d1e6b56256f6120467a4d58734c2f17683
0972251 <44231826+0972251@users.noreply.github.com>
2019-12-16 22:23:49 +0100

    image pionnen import

--- game_screen.py
+++ game_screen.py
@@ -3,6 +3,7 @@ from screen import Screen
 from button import Button
 from dice_manager import DiceManager
 from location_button  import LocationButton
+from player_button import PlayerButton
 
 class GameScreen(Screen):
     
@@ -11,6 +12,14 @@ class GameScreen(Screen):
     
     boardImageFour = None
     boardImageSix = None
+    
+
+    pionRoodR = None
+    pionRoodL = None
+    pionBlauwL = None
+    pionBlauwR = None
+    pionGroenL = None
+    pionGroenR = None
     
     fromScreen = None
     player = None
@@ -21,6 +30,13 @@ class GameScreen(Screen):
         
         self.boardImageFour = loadImage('board-players4.png')
         self.boardImageSix = loadImage('board-players6.png')
+        
+        self.pionRoodL = loadImage('RoodL.png')
+        self.pionRoodR = loadImage('RoodR.png')
+        self.pionBlauwL = loadImage('BlauwL.png')
+        self.pionBlauwR = loadImage('BlauwR.png')
+        self.pionGroenL = loadImage('GroenL.png')
+        self.pionGroenR = loadImage('GroenR.png')
         
         self.boardX = width / 2 - (self.boardWidth / 3.2)
         self.boardY = 40
@@ -41,6 +57,14 @@ class GameScreen(Screen):
         
         for player in playerManager.getPlayers():
             print(player.getLocation())
+            
+        image(self.pionRoodR, 50, 150, 40, 50)
+        image(self.pionRoodL, 50, 210, 40, 50)
+        image(self.pionBlauwL, 100, 150, 40, 50)
+        image(self.pionBlauwR, 100, 210, 40, 50)
+        image(self.pionGroenL, 150, 150, 40, 50)
+        image(self.pionGroenR, 150, 210, 40, 50)
+            
         
         boardImage = self.boardImageSix if playerManager.maxPlayers == 6 else self.boardImageFour
         image(boardImage, self.boardX, self.boardY, self.boardWidth, self.boardHeight)
