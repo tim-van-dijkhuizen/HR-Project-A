523c2dd7cb5adb7adde0ff656a2c8eb0af995b05
0972251 <44231826+0972251@users.noreply.github.com>
2019-12-30 19:40:56 +0100

    botManager upgrade 2

--- bot_manager.py
+++ bot_manager.py
@@ -3,6 +3,7 @@ from module import Module
 class BotManager(Module):
     
     currentBot = None
+    botPartner = None
     
     def getHandle(self):
         return 'botManager'
@@ -17,27 +18,27 @@ class BotManager(Module):
         diceManager = self.app.getModule('diceManager')
         playerManager = self.app.getModule('playerManager')
         steps = diceManager.diceValue
+        # er moet hier komen dat de bot naar zijn partner automatisch moet gaan
         
-        
-    def botBuddy(self):
-        # turnManager = self.app.getModule('turnManager')
-        # playerManager = self.app.getModule('playerManager')
-        # bot = playerManager.botPlayer
-        # players = playerManager.getPlayers()
-        # botPlayer = None if bot == None else players.index(self.Bot)
-        # botBuddy = botPlayer - 1 if bot % botPlayer == 0 else botPlayer + 1
-        
+    def botLocation(self):
         playerManager = self.app.getModule('playerManager')
         players = playerManager.getPlayers()
+        bot = playerManager.botPlayer
+        for player in players:
+            if player == bot:
+                botLocation = player.playerManager.getLocation()
+        
+    def Partner(self):        
+        playerManager = self.app.getModule('playerManager')
+        players = playerManager.getPlayers()
+        bot = playerManager.botPlayer
         currentIndex = 0 if self.currentBot == None else players.index(self.currentBot)
-        evenIndex = currentIndex % 2 == 0
-        oddIndex = currentIndex % 2 == 1
+        self.botPartner = currentIndex - 1 if bot % 2 == 0 else currentIndex + 1
+        for player in players:
+            if player == self.botPartner:
+                player.playerManager.getLocation()
+
                     
-        #reset index if max players exceeded
-        if nextIndex >= len(players):
-            nextIndex = 0
-            
-        self.currentPlayer = playerManager.getPlayer(nextIndex)
         
         
         
