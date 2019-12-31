7d4212979e310812f15f53bdf5aaede6a11c6daa
0972251 <44231826+0972251@users.noreply.github.com>
2019-12-31 16:07:05 +0100

    botManager update 4

--- bot_manager.py
+++ bot_manager.py
@@ -14,38 +14,13 @@ class BotManager(Module):
     def draw(self):
         playerManager = self.app.getModule('playerManager')
         
-    def makingSteps(self):
-        diceManager = self.app.getModule('diceManager')
-        playerManager = self.app.getModule('playerManager')
-        steps = diceManager.diceValue
-        # er moet hier komen dat de bot naar zijn partner automatisch moet gaan
-        if botLocation > partnerLocation:
-            botLocation = steps + botLocation
-        else:
-            botLocation = steps - botLocation
-        
     def botLocation(self):
         playerManager = self.app.getModule('playerManager')
-        players = playerManager.getPlayers()
         botLocation = playerManager.botPlayer.getLocation()
         return botLocation
-
-    def partner(self):        
+            
+    def partnerLocation(self):        
         playerManager = self.app.getModule('playerManager')
-        players = playerManager.getPlayers()
-        bot = playerManager.botPlayer
-        currentIndex = 0 if self.currentBot == None else players.index(self.currentBot)
-        if bot % 2 == 0: 
-            self.botPartner = currentIndex - 1
-        else:
-            self.botPartner = currentIndex + 1
-        for player in players:
-            if player == self.botPartner:
-                partnerLocation = player.playerManager.getLocation()
-                return partnerLocation
-
-
-                    
-        
-        
-        
+        partnerLocation = self.botPartner.getLocation()
+        return partnerLocation
+    
