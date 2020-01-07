44c90fa118c66b542a7fe7a5f5ba844d4747dc95
SmokingIsBadMkay <timvandijkhuizen@hotmail.nl>
2020-01-07 22:33:47 +0100

    Cleanup and set box configs on start

--- game_manager.py
+++ game_manager.py
@@ -5,13 +5,27 @@ from card_screen import CardScreen
 
 class GameManager(Module):
     
-    # Locations of all good and bad cards
-    goodCardLocation = [Location(1, 5), Location(1, 9), Location(1, 13), Location(1, 17), Location(1, 21), Location(2, 5), Location(2, 9), Location(2, 13), Location(2, 17), Location(2, 21), Location(3, 5), Location(3, 9),Location(3, 13), Location(3, 17), Location(3, 21), Location(4, 5), Location(4, 9), Location(4, 13), Location(4, 17), Location(4, 21), Location(5, 5), Location(5, 9), Location(5, 13), Location(5, 17), Location(5, 21), Location(6, 5), Location(6, 9), Location(6, 13), Location(6, 17), Location(6, 21)]
+    # Goodcard locations
+    goodCardLocation = [
+        Location(1, 5), Location(1, 9), Location(1, 13),
+        Location(1, 17), Location(1, 21), Location(2, 5),
+        Location(2, 9), Location(2, 13), Location(2, 17),
+        Location(2, 21), Location(3, 5), Location(3, 9),
+        Location(3, 13), Location(3, 17), Location(3, 21),
+        Location(4, 5), Location(4, 9), Location(4, 13),
+        Location(4, 17), Location(4, 21), Location(5, 5),
+        Location(5, 9), Location(5, 13), Location(5, 17),
+        Location(5, 21), Location(6, 5), Location(6, 9),
+        Location(6, 13), Location(6, 17), Location(6, 21)
+    ]
+    
+    # Dynamic locations
     badCardLocation = None
     minBoxesLocation = None
     addBoxesLocation = None
     warpsLocation = None
     breakPointsLocation = None
+    
     breakPoint = False
 
     def getHandle(self):
@@ -30,7 +44,7 @@ class GameManager(Module):
                 continue
                 
             # Check if they are standing on a breakpoint
-            if player.getLocation() == 41 or player.getLocation() == 85:
+            if player.getLocation() in self.breakPointsLocation:
                 self.showLoseScreen()
             else :
                 self.showWinScreen()
@@ -50,7 +64,6 @@ class GameManager(Module):
             self.warpsLocation = [[Location(1, 6), Location(1, 10)], [Location(1, 18),Location(1, 14)], [Location(2, 4), Location(2, 19)], [Location(2, 6), Location(2, 10)], [Location(2, 18),Location(2, 14)], [Location(3, 6), Location(3, 10)], [Location(3, 18),Location(3, 14)],[Location(4, 4), Location(4, 19)], [Location(4, 6), Location(4, 10)], [Location(4, 18),Location(4, 14)]]
             self.breakPointsLocation = [Location(2,19), Location(4,19)]
             self.badCardsLocation = [Location(1, 3), Location(1, 7), Location(1, 11), Location(1, 15), Location(1, 19), Location(2, 3), Location(2, 7), Location(2, 11), Location(2, 15), Location(3, 3), Location(3, 7), Location(3, 11), Location(3, 15), Location(3, 19), Location(4, 3), Location(4, 7), Location(4, 11), Location(4, 15)]
-
         else:
             self.minBoxesLocation = [Location(3,12), Location(6,12)]
             self.addBoxesLocation = [Location(1,12), Location(2,12), Location(4,12), Location(5,12)]    
@@ -58,17 +71,17 @@ class GameManager(Module):
             self.breakPointsLocation = [Location(3, 19), Location(6, 19)]
             self.badCardsLocation = [Location(1, 3), Location(1, 7), Location(1, 11), Location(1, 15), Location(1, 19), Location(2, 3), Location(2, 7), Location(2, 11), Location(2, 15), Location(2, 19), Location(3, 3), Location(3, 7), Location(3, 11), Location(3, 15), Location(4, 3), Location(4, 7), Location(4, 11), Location(4, 15), Location(4, 19), Location(5, 3), Location(5, 7), Location(5, 11), Location(5, 15), Location(5, 19), Location(6, 3), Location(6, 7), Location(6, 11), Location(6, 15)]
             
-    def controlGoodCard(self):
+    def checkGoodCard(self):
         turnManager = self.app.getModule('turnManager')
         
-        if turnManager.currentPlayer.getLocation() == self.goodCardLocation:
+        if turnManager.currentPlayer.getLocation() in self.goodCardLocation:
             goodLocation = cardScreen.steps + turnManager.currentPlayer.getLocation()
             cardScreen = self.app.getScreen('card')
             
             self.app.setCurrentScreen(cardScreen)
             turnManager.currentPlayer.setLocation(goodLocation)
             
-    def controlBadCard(self):
+    def checkBadCard(self):
         turnManager = self.app.getModule('turnManager')
         
         if turnManager.currentPlayer.getLocation() == self.badCardLocation:
@@ -78,7 +91,7 @@ class GameManager(Module):
             self.app.setCurrentScreen(cardScreen)
             turnManager.currentPlayer.setLocation(badLocation)
     
-    def controlBreakpoint(self):
+    def checkBreakpoint(self):
         playerManager = self.app.getModule('playerManager')
         botManager = self.app.getModule('botManager')
         
