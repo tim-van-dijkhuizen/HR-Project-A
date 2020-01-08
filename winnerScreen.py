47bc83d60d6f2659823e7db17aeb749cb6385d0f
canerdokuyucu <0983581@hr.nl>
2020-01-08 09:03:08 +0100

    created winnerScreen

--- winnerScreen.py
+++ winnerScreen.py
@@ -1,19 +1,18 @@
 def setup(): 
-    global a, font , score, img
+    global a, font, 
     size(1200, 650)
     textSize(150)
     font = createFont("Segoe script",  60) 
-    score = 0
     
 def draw(): 
-    global message, img, font
+    global message, font
     background(255, 74, 113)
     textFont(font)
     textAlign(CENTER)
     fill(0)
     text('Congratulations you won !', 600, 300)
     
-    fontt = createFont('Arial Black', 35)
+    font = createFont('Arial Black', 35)
     textFont(fontt)
     textSize(35)
     text('Thanks for playing our game !', 600, 550)
